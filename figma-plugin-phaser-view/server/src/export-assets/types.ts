2747afbe7831c27d102cf4b5485d944055d64536
Sergey Tokarev <stokarev@novakidschool.com>
2026-04-22 18:56:16 +0700

    IAutoViewData - final fix button origin

--- figma-plugin-phaser-view/server/src/export-assets/types.ts
+++ figma-plugin-phaser-view/server/src/export-assets/types.ts
@@ -36,6 +36,9 @@ export interface PosObject {
   setPosition(x: number, y: number): this;
 }
 
+
+export type PosGameObject = Phaser.GameObjects.GameObject & PosObject;
+
 export interface IAutoViewAssetChildData {
   readonly type?: "asset";
   readonly asset: IAutoAssetData;
